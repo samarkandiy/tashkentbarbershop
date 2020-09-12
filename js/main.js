a15e111bf56dec36d8886dcb745065cc4d8d2adc
Avaz Bokiev <html5web@gmail.com>
2020-09-12 18:20:24 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$('.hamburger, .menu').click(function () {
+    if ($('.menu').is(':hidden')) {
+        $('.menu').show('slideToggle');
+    } else {
+        $('.menu').hide('slideToggle');
+    }
+});
+
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function (e) {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+});
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
+
+var mySwiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 5000,
+    },
+});
